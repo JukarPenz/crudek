177bdc8195d6b720963ef637bb8c7927bb9f1c26
Carlos Pecina <jukarpenz@gmail.com>
2023-07-28 15:53:44 -0600

    boot

---
+++ src/main/java/com/demo/crudej/CrudejApplication.java
@@ -0,0 +1,13 @@
+package com.demo.crudej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudejApplication.class, args);
+	}
+
+}
